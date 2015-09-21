a7383af42dea6f191729383c96f5286565f38817
Max Mastalerz <meaniostack@gmail.com>
2015-09-21 17:09:51 -0400

    improved error checking in room creator

--- roomCreator.js
+++ roomCreator.js
@@ -1,6 +1,31 @@
 var meth = require('./meth.js');
 var join = require('./join.js');
 
+function noCreationErrors(client, rooms, room, maxPlayers, mapSize, mapVisibility, bombDelay) {
+	var sentMessage = '';
+	if(room==='null' || room==='[object Object]' || room==='undefined' || room==='' || (/^\s+$/.test(room))) {
+		sentMessage+='Room name must not be blank\n';
+	} if(!meth.roomAvailable(rooms, room)) {
+		sentMessage+='This noom name must be unique\n';
+	} if(room.length>15) {
+		sentMessage+='The length of the name surpassed 15 chars\n';
+	} if(maxPlayers<2 || maxPlayers>6) {
+		sentMessage+='Max players was outside the 2-6 range\n';
+	} if(mapSize<10 || mapSize>70) {
+		sentMessage+='Map size was outside the 10-70 range\n';
+	} if(mapVisibility<5 || mapVisibility>35) {
+		sentMessage+='Map visibility was outside the 5-35 range\n';
+	} if(bombDelay<500 || bombDelay>3000) {
+		sentMessage+='bombDelay was outside the 500-3000 range\n';
+	}
+
+	if(sentMessage==='') {
+		return true;
+	} else {
+		client.emit('errorMessage', 'Issues that occured when creating room: \n'+sentMessage);
+	}
+}
+
 module.exports = function(io, client, rooms, room, maxPlayers, mapSize, mapVisibility, bombDelay, roomPassword) {
 	room = meth.sanitizeInput(room);
 	if(roomPassword!==undefined) { roomPassword = meth.sanitizeInput(roomPassword); }
@@ -11,38 +36,27 @@ module.exports = function(io, client, rooms, room, maxPlayers, mapSize, mapVisib
 	if(mapVisibility>mapSize) {	mapVisibility = mapSize; }
 	bombDelay = parseInt(bombDelay) || 1500;
 
-	if(room!=='null' && room!=='[object Object]' && room!=='undefined' && room!=='' && !(/^\s+$/.test(room))) {
-		room = meth.sanitizeInput(room.replace(/\s+/g,' ')).trim();
-		if(meth.roomAvailable(rooms, room) && room.length<=15) {
-			if(maxPlayers>=2 && maxPlayers<=6 && mapSize<=70 && mapSize>=10 && mapVisibility>=5 && mapVisibility<=35 && bombDelay>=500 && bombDelay<=3000) {
-				rooms[room] = {
-					players: {},
-					playerCount: 0,
-					maxPlayers: maxPlayers,
-					mapSize: mapSize,
-					mapVisibility: mapVisibility,
-					bombDelay: bombDelay,
-					roomPassword: roomPassword,
-					map: meth.Create2DArray(mapSize)
-				};
+	if(noCreationErrors(client, rooms, room, maxPlayers, mapSize, mapVisibility, bombDelay)) {
+		rooms[room] = {
+			players: {},
+			playerCount: 0,
+			maxPlayers: maxPlayers,
+			mapSize: mapSize,
+			mapVisibility: mapVisibility,
+			bombDelay: bombDelay,
+			roomPassword: roomPassword,
+			map: meth.Create2DArray(mapSize)
+		};
 
-				for(var y=0;y<rooms[room].mapSize;y++) {
-					for(var x=0;x<rooms[room].mapSize;x++) {
-						if(y===0 || y===rooms[room].mapSize-1 || x===0 || x===rooms[room].mapSize-1) {
-							rooms[room].map[y][x] = '2';
-						} else {
-							rooms[room].map[y][x] = (Math.floor(Math.random()*2)+0).toString();	//'0' or '1' tile
-						}
-					}
+		for(var y=0;y<rooms[room].mapSize;y++) {
+			for(var x=0;x<rooms[room].mapSize;x++) {
+				if(y===0 || y===rooms[room].mapSize-1 || x===0 || x===rooms[room].mapSize-1) {
+					rooms[room].map[y][x] = '2';
+				} else {
+					rooms[room].map[y][x] = (Math.floor(Math.random()*2)+0).toString();	//'0' or '1' tile
 				}
-				join(io, client, rooms, room);
-			} else {
-				client.emit('errorMessage', 'Some room variables were not within the correct limits.');
 			}
-		} else {
-			client.emit('errorMessage', 'The room name must be unique, and must not surpass 15 chars');
 		}
-	} else {
-		client.emit('errorMessage', 'The room name cannot be blank');
+		join(io, client, rooms, room);
 	}
 };
